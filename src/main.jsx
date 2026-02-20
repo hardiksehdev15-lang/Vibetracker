fc1669235ea1723ec1a461b4ce4cae6b6da166ac
nidhanmahajan <Nidhanmahajan23@gmail.com>
2026-02-20 21:46:53 +0530

    Complete login page integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import DashboardPage from './DashboardPage'; 
+import LoginPage from './LoginPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+<React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* This makes the Dashboard your home page */}
         <Route path="/" element={<DashboardPage />} />
-        
-        {/* This is a placeholder for your login page */}
-        <Route path="/login" element={<div className="text-white p-10">Please Login (Login Page coming soon)</div>} />
-        
-        {/* Redirect any unknown paths to the home page */}
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/login" element={<LoginPage />} /> {/* 2. Replace the div with LoginPage */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
